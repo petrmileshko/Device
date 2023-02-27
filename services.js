92901092e0966299e0ae5e5a8133699dc9348f50
Peter <mileshko.petr@yandex.ru>
2023-02-27 18:26:40 +0300

    completed services toggling tabs

--- services.js
+++ services.js
@@ -4,7 +4,7 @@ let descriptions = document.querySelectorAll('.service-description');
 function serviceToggle(service) {
   service.addEventListener('click', (event) => {
     event.preventDefault();
-    // console.log(service.querySelector('.service-link').getAttribute('href').slice(1));
+
     if (!service.classList.contains('service-current')) {
       document.querySelector('.description-current').classList.remove('description-current');
       document.querySelector('.service-current').classList.remove('service-current');
